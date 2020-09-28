602f5238bac7e28928d547948692f951afcb2b9a
Stanley Wang <swang@Stanley-Macbook-Pro.local>
2020-09-28 23:13:18 +0800

    downgrade graphql

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Subscription from "./resolvers/Subscription";
 import User from "./resolvers/User";
 import Post from "./resolvers/Post";
 import Comment from "./resolvers/Comment";
+import './prisma'
 
 const pubsub = new PubSub();
 
