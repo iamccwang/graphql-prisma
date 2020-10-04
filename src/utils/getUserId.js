38b1d04b1d20b342ad148eb8a70e1938a4ec4c2f
Stanley Wang <swang@Stanley-Macbook-Pro.local>
2020-10-04 10:43:59 +0800

    Update JWT_SECRET

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -7,7 +7,7 @@ const getUserId = (request, requireAuth = true) => {
 
     if (header) {
         const token = header.replace('Bearer ', '')
-        const decoded = jwt.verify(token, process.env.PRISMA_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         return decoded.userId
     }
 
