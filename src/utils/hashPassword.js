3720e82901f8c7edbe81e9d3c5bd7bf1f92f09a0
Stanley Wang <swang@Stanley-Macbook-Pro.local>
2020-10-04 10:53:04 +0800

    fix wrong variable

--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs'
 
-const hashPassword = (passwrod) => {
+const hashPassword = (password) => {
     if (password.length < 8) {
         throw new Error('Password must be 8 characters or longer')
     }
